01f197956e172e20d8072ee5fc15dd005b66f9bf
arihantkode <ak3536@columbia.edu>
2023-07-23 14:40:55 -0700

    barebones website.

---
+++ src/data/user.js
@@ -0,0 +1,38 @@
+const INFO = {
+	main: {
+		title: "Arihant Kochhar",
+		name: "Arihant K",
+		email: "arihant8kochhar@gmail.com",
+		logo: "../../fotor-ai.jpg",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/_arihant_",
+		github: "https://github.com/arihantkode",
+		linkedin: "https://www.linkedin.com/in/arihant-kochhar-45a78b39/",
+		instagram: "https://www.instagram.com/seeker___voyager___seer/",
+		facebook: "https://www.facebook.com/arihant.kochhar/",
+	},
+
+	homepage: {
+		title: "Arihant",
+		description:
+			"this is some description about me.",
+	},
+
+	about: {
+		title: "I'm Arihant",
+		description:
+			"this is some description about me.",
+	},
+
+	articles: {
+		title: "",
+		description:
+			"",
+	},
+
+	projects: [],
+};
+
+export default INFO;
